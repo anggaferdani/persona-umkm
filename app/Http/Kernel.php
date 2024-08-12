449980ff452be43d88e0a17d48b28cb6a1ff1d4a
anggaferdani <eanggawahyu@gmail.com>
2024-08-12 20:49:33 +0700

    middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'disableBackButton' => \App\Http\Middleware\DisableBackButton::class,
+        'disableRedirect' => \App\Http\Middleware\DisableRedirect::class,
         'authenticated' => \App\Http\Middleware\Authenticated::class,
         'superadmin' => \App\Http\Middleware\Superadmin::class,
         'admin' => \App\Http\Middleware\Admin::class,
