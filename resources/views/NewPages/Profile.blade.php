ad8907c74507bd8a151d63128de630692655f0e0
anggaferdani <eanggawahyu@gmail.com>
2024-09-12 06:38:02 +0700

    fix

--- resources/views/NewPages/Profile.blade.php
+++ resources/views/NewPages/Profile.blade.php
@@ -12,16 +12,16 @@
         <div class="row justify-content-md-start justify-content-center">
             <div class="col-md-4 col-6 ">
                 @if($profil)
-                <img class="orang" src="{{asset('img/'.$profil->foto ?? '-')}}" alt="orang">
+                <img class="orang" src="{{asset('img/'.$profil->foto ?? '')}}" alt="orang">
                 @else
                 <img class="orang" src="{{asset('images/profile.jpg')}}" alt="orang">
                 @endif
                 <a class="btn w-100 mt-1 ms-auto text-white" href="/logout" role="button" style="background-color: #2388FF;">LOG OUT</a>
             </div>
             <div class="col-md-8" id="descProfile">
-                <h3 class="text-md-start text-dark text-center mb-1">{{Auth::user()->name ?? '-'}}</h3>
-                <div class="text-md-start text-dark text-center mb-1">{{Auth::user()->email ?? '-'}}</div>
-                <div class="text-md-start text-dark text-center mb-3">{{Auth::user()->no_telp ?? '-'}}</div>
+                <h3 class="text-md-start text-dark text-center mb-1">{{Auth::user()->name ?? ''}}</h3>
+                <div class="text-md-start text-dark text-center mb-1">{{Auth::user()->email ?? ''}}</div>
+                <div class="text-md-start text-dark text-center mb-3">{{Auth::user()->no_telp ?? ''}}</div>
 
                 <div class="address_profile text-black">
                     @if(!$profil)
@@ -31,8 +31,8 @@
                     @else
                         @if(Auth::user()->role == 3)
                         <h5 class="text-md-start text-center fw-bold mb-3">Alamat & Deskripsi UKM :</h5>
-                        <p class="text-md-start text-center mb-3">{{$profil->alamat ?? '-'}}</p>
-                        <p class="text-md-start text-center mb-3">{{$profil->deskripsi ?? '-'}}</p>
+                        <p class="text-md-start text-center mb-3">{{$profil->alamat ?? ''}}</p>
+                        <p class="text-md-start text-center mb-3">{{$profil->deskripsi ?? ''}}</p>
 
                         <div class="d-flex justify-content-center justify-content-md-start">
                             <button type="button" class="btn btn-sm btn-primary mt-2 fw-bold" data-bs-toggle="modal" data-bs-target="#exampleModaProfil">
@@ -41,7 +41,7 @@
                         </div>
                         @elseif(Auth::user()->role == 4)
                         <h4 class="fw-bold">Alamat :</h4>
-                        <p class="text-md-start text-center mb-3">{{$profil->alamat ?? '-'}}</p>
+                        <p class="text-md-start text-center mb-3">{{$profil->alamat ?? ''}}</p>
 
                         <div class="d-flex justify-content-center justify-content-md-start">
                             <button type="button" class="btn btn-sm btn-primary mt-2 fw-bold" data-bs-toggle="modal" data-bs-target="#exampleModaProfil">
@@ -68,22 +68,22 @@
             {{ csrf_field() }}
             <div class="form-group mb-3">
                 <label for="exampleInputUsername1">Alamat Anda<span class="text-danger">*</span></label>
-                <input type="text" class="form-control" id="exampleInputUsername1" placeholder="Input Alamat UKM Anda..." name="alamat" value="{{ $profil->alamat ?? '-' }}">
+                <input type="text" class="form-control" id="exampleInputUsername1" placeholder="Input Alamat UKM Anda..." name="alamat" value="{{ $profil->alamat ?? '' }}">
                 @error('alamat')
                         <p class="text-danger">{{ $message }}</p>
                 @enderror
             </div>
             <div class="form-group mb-3">
                 <label for="exampleInputUsername1">Deskripsi UKM Anda<span class="text-danger">*</span></label>
-                <textarea class="form-control" style="height: 200px" placeholder="Input Deskripsi UKM Anda..." id="floatingTextarea" name="deskripsi">{{ $profil->deskripsi ?? '-' }}</textarea>
+                <textarea class="form-control" style="height: 200px" placeholder="Input Deskripsi UKM Anda..." id="floatingTextarea" name="deskripsi">{{ $profil->deskripsi ?? '' }}</textarea>
                 @error('deskripsi')
                         <p class="text-danger">{{ $message }}</p>
                 @enderror
             </div>
             <div class="form-group mb-3">
                 <label for="exampleInputUsername1">Foto Profil<span class="text-danger">* ukuran 1:1</span></label>
-                <input type="file" class="form-control" id="exampleInputUsername1" name="foto" value="{{ $profil->foto ?? '-' }}">
-                <a href="/img/{{ $profil->foto ?? '-' }}" target="_blank">{{ $profil->foto ?? '-' }}</a>
+                <input type="file" class="form-control" id="exampleInputUsername1" name="foto" value="{{ $profil->foto ?? '' }}">
+                <a href="/img/{{ $profil->foto ?? '' }}" target="_blank">{{ $profil->foto ?? '' }}</a>
                 @error('foto')
                         <p class="text-danger">{{ $message }}</p>
                 @enderror
@@ -100,15 +100,15 @@
             {{ csrf_field() }}
             <div class="form-group mb-3">
                 <label for="exampleInputUsername1">Alamat Anda<span class="text-danger">*</span></label>
-                <input type="text" class="form-control" id="exampleInputUsername1" placeholder="Input Alamat UKM Anda..." name="alamat" value="{{ $profil->alamat ?? '-' }}">
+                <input type="text" class="form-control" id="exampleInputUsername1" placeholder="Input Alamat UKM Anda..." name="alamat" value="{{ $profil->alamat ?? '' }}">
                 @error('alamat')
                         <p class="text-danger">{{ $message }}</p>
                 @enderror
             </div>
             <div class="form-group mb-3">
                 <label for="exampleInputUsername1">Foto Profil<span class="text-danger">* ukuran 1:1</span></label>
-                <input type="file" class="form-control" id="exampleInputUsername1" name="foto" value="{{ $profil->foto ?? '-' }}">
-                <a href="/img/{{ $profil->foto ?? '-' }}" target="_blank">{{ $profil->foto ?? '-' }}</a>
+                <input type="file" class="form-control" id="exampleInputUsername1" name="foto" value="{{ $profil->foto ?? '' }}">
+                <a href="/img/{{ $profil->foto ?? '' }}" target="_blank">{{ $profil->foto ?? '' }}</a>
                 @error('foto')
                         <p class="text-danger">{{ $message }}</p>
                 @enderror
